61841f9481b0b0967904fc0813c267bd2568a870
tbircek <tbircekhitechled@gmail.com>
2019-08-13 11:40:21 -0400

    daily update

---
+++ metering/Controls/BaseUserControl.cs
@@ -0,0 +1,45 @@
+﻿
+using System.ComponentModel;
+using System.Windows.Controls;
+using metering.core;
+
+namespace metering.Controls
+{
+    /// <summary>
+    /// The base class for content inside <see cref="TestDetailsPage"/>
+    /// </summary>
+    public class BaseUserControl: UserControl
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The control
+        /// </summary>
+        private TestDetailsPage mUserControl;
+
+        #endregion
+
+        #region Public Properties
+
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BaseUserControl()
+        {
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                mUserControl = new TestDetailsPage
+                {
+                    ViewModel = new TestDetailsViewModel()
+                };
+            }
+        }
+
+        #endregion
+    }
+}
