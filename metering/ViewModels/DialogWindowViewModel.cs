8f0485ef3bd68b9a4729fa007417da24696c58d5
tbircek <tbircekhitechled@gmail.com>
2019-08-13 11:41:09 -0400

    daily update

---
+++ metering/ViewModels/DialogWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace metering
+{
+    /// <summary>
+    ///The view model for the test details
+    /// </summary>
+   public class DialogWindowViewModel: WindowViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The title of this dialog
+        /// </summary> 
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The content to host inside dialog
+        /// </summary>
+        public Control Content { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DialogWindowViewModel(Window window): base(window) { }
+
+        #endregion
+    }
+}
