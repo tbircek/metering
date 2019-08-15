187a47bf2ddebcea5b2c98de273e6be2a0b90fe9
tbircek <tbircekhitechled@gmail.com>
2019-08-15 17:55:59 -0400

    daily update

---
+++ metering/AttachedProperties/AutoScrollToBottomProperty.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace metering
+{
+    /// <summary>
+    /// Scroll to bottom of the control to show the most up to date information
+    /// </summary>
+    public class AutoScrollToBottomProperty: BaseAttachedProperty<AutoScrollToBottomProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            //// don't show if it in design mode
+            //if (DesignerProperties.GetIsInDesignMode(sender))
+            //    return;
+
+            // verify sender is a ScrollViewer
+            if (!(sender is ScrollViewer control))
+                 return;
+
+            // Remove older handle
+            control.ScrollChanged -= Control_ScrollChanged;
+
+            // Scroll content to bottom
+            control.ScrollChanged += Control_ScrollChanged;
+        }
+
+        private void Control_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var scroll = sender as ScrollViewer;
+
+            // If it close to the bottom
+            if (scroll.ScrollableHeight - scroll.VerticalOffset < 5)
+                // Scroll to the bottom
+                scroll.ScrollToEnd();
+        }
+    }
+}
