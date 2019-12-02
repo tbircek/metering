07ae8b8720004035b8ee66076ee3b852111a364f
tbircek <tbircek@beckwithelectric.com>
2019-12-02 13:28:03 -0500

    adds a new "SettingsPage"

---
+++ metering.wpf/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using metering.core;
+
+namespace metering
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsViewPage : BasePage<SettingsViewModel>
+    {
+        public SettingsViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
