3a8bc26e91d26e2efb0e6ada24f14315a532a01b
tbircek <tbircek@beckwithelectric.com>
2019-12-04 13:21:28 -0500

    adds new Omicron Hardware Outputs view, models, controls and page

---
+++ metering.wpf/Controls/SettingsListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace metering
+{
+    /// <summary>
+    /// Interaction logic for SettingsListItemControl.xaml
+    /// </summary>
+    public partial class SettingsListItemControl : UserControl
+    {
+        public SettingsListItemControl()
+        {
+            InitializeComponent();            
+        }
+    }
+}
