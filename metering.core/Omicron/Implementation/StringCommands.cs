e48937d76ff8a109ce9fe750af1db3f476e91a80
tbircek <tbircek@beckwithelectric.com>
2020-01-17 08:28:26 -0500

    adds new Harmonics wave form string to StringBuilder()

--- metering.core/Omicron/Implementation/StringCommands.cs
+++ metering.core/Omicron/Implementation/StringCommands.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,16 +30,29 @@ namespace metering.core
         /// Sends "ON" command to Omicron Test Set.
         /// </summary>
         /// <remarks>
-        /// Command format:
-        /// "out:[ana:]v|i(generator_list):[sig(no):]signalType(fValue) with omitted optional "step" parameter.
+        /// A harmonic is defined by the three parameters <harmonicX>,<amplitude_factorX>
+        /// and<phaseX>.They define the order of the harmonic, its amplitude relative to the
+        /// setting of the a() command and its phase relative to the fundamental.For the
+        /// definition of every harmonic you must define its three parameters<harmonicX>,
+        /// <amplitude_factorX> and<phaseX>.
         /// </remarks>
+        /// <example>
+        /// Output strings:
+        /// ex 1: out:ana:v(1:1):a(120);p(0);f(60);wav(sin)
+        /// ex 2: out:ana:v(1:1):a(120);p(0);f(60);wav(sum,1,2,0.10,0) --> 2nd harmonics @ 10%
+        /// ex 3: out:ana:v(1:1):a(120);p(0);f(60);wav(sum,1,2,0.10,0,3,0.05,0,4,0.20,0) --> 2nd @ 10%, 3rd @ 5%, 4th @ 20%
+        /// </example>
         /// <param name="generatorType">Triple list type: "v" for Voltage, "i" for current amplifier.</param>
         /// <param name="tripletNumber">This parameter is 1 or 2 and selects either signal component 1 or component 2. Ex: "1:1".</param>
         /// <param name="amplitude">Magnitude of analog signal.</param>
         /// <param name="phase">Phase of analog signal.</param>
         /// <param name="frequency">Frequency of analog signal.</param>
-        /// <example>Output string ex: out:ana:v(1:1):a(120);p(0);f(60);wav(sin)</example>
-        public async void SendOutAnaAsync(char generatorType, string tripletNumber, double amplitude, double phase, double frequency)
+        /// <param name="amplitude_factor">Harmonics ramping only. Sets the amplitude of the fundamental relative to the setting of the a() command.</param>
+        /// <param name="harmonicX">Harmonics ramping only. Defines the order of a harmonic. e.g 2 = 2nd harmonic</param>
+        /// <param name="amplitudeFactorX">Harmonics ramping only. The amplitude set with the a() command is multiplied with <amplitude_factorX> to
+        /// obtain the amplitude of the fundamental or harmonic respectively.</param>
+        /// <param name="phaseX">Harmonics ramping only. Defines the phase of the harmonic.</param>
+        public async void SendOutAnaAsync(char generatorType, string tripletNumber, double amplitude, double phase, double frequency, double amplitude_factor, int harmonicX, double amplitudeFactorX, double phaseX)
         {
             try
             {
@@ -48,11 +62,23 @@ namespace metering.core
                     // check if the user canceling test
                     if (!IoC.Commands.Token.IsCancellationRequested)
                     {
+                        // default waveform to use is "sin"
+                        string waveForm = "sin";
+                        // only time this changes while the ramping signal is "Harmonics"
+                        if (IoC.TestDetails.IsHarmonics)
+                        {
+                            // use waveform == "sum" 
+                            // e.g. sum,1,2,0.10,0
+                            waveForm = $"sum,{amplitude_factor},{harmonicX},{amplitudeFactorX/100.0d:F6},{phaseX:F6}";
+
+                        }
+
                         // is frequency zero? yes == use NominalFrequency no == frequency
                         double frequencyToApply = frequency.Equals(0) ? NominalFrequency : frequency;
 
+
                         // build a string to send to Omicron Test set
-                        StringBuilder stringBuilder = new StringBuilder($"out:ana:{generatorType}({tripletNumber}):{nameof(SignalType.a)}({amplitude});{nameof(SignalType.p)}({phase});{nameof(SignalType.f)}({frequencyToApply});wav(sin)");
+                        StringBuilder stringBuilder = new StringBuilder($"out:ana:{generatorType}({tripletNumber}):{nameof(SignalType.a)}({amplitude});{nameof(SignalType.p)}({phase});{nameof(SignalType.f)}({frequencyToApply});wav({waveForm})");
 
                         // update the log
                         IoC.Logger.Log($"device id: {IoC.CMCControl.DeviceID} -- command: {stringBuilder}", LogLevel.Informative);
