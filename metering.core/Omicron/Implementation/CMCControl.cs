e11b4da90dc3bc9f5a8daaf71c12e399d98845e1
tbircek <tbircek@beckwithelectric.com>
2020-01-17 08:25:12 -0500

    adds harmonics file name scheme, increases Omicron TurnOnCMC start delay

--- metering.core/Omicron/Implementation/CMCControl.cs
+++ metering.core/Omicron/Implementation/CMCControl.cs
@@ -240,7 +240,7 @@ namespace metering.core
                             await IoC.Task.Run(() => IoC.SetOmicron.SendOmicronCommands(SignalName, testStartValue));
 
                             // delay little bit Omicron CMC power up
-                            await Task.Delay(200);
+                            await Task.Delay(1000);
 
                             // Turn On Omicron Analog Outputs per the user input
                             await IoC.Task.Run(() => IoC.PowerOptions.TurnOnCMC());
@@ -297,13 +297,13 @@ namespace metering.core
                             if (string.IsNullOrWhiteSpace(IoC.TestDetails.TestFileName))
                             {
                                 // test result file name contains Register, From, To, and test start time values
-                                testDetailsFileName = $"{IoC.TestDetails.Register}_{From:F6}-{To:F6}_{fileId}";
+                                testDetailsFileName = $"[{(IoC.TestDetails.IsHarmonics ? IoC.Communication.TestingHarmonicOrder.ToString() : string.Empty)}]{IoC.TestDetails.Register}_{From:F6}-{To:F6}_{fileId}";
                             }
                             else
                             {
                                 // test result file name contains "Test File Name" per the user input.
                                 // file name might contain multiple "."
-                                testDetailsFileName = $"{IoC.TestDetails.TestFileName.Substring(0,IoC.TestDetails.TestFileName.LastIndexOf('.'))}_{fileId}";
+                                testDetailsFileName = $"[{(IoC.TestDetails.IsHarmonics?IoC.Communication.TestingHarmonicOrder.ToString(): string.Empty)}]{IoC.TestDetails.TestFileName.Substring(0,IoC.TestDetails.TestFileName.LastIndexOf('.'))}_{fileId}";
                             }
 
                             // create a TestResultLogger to generate a test report in .csv format.
