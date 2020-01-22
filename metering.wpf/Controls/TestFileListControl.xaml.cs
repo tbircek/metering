8856a7ec1c178d6d82d9ff904a1f424ffd8f44b3
tbircek <tbircek@beckwithelectric.com>
2020-01-22 09:37:29 -0500

    adds multi-test interface to Communication view

---
+++ metering.wpf/Controls/TestFileListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace metering
+{
+    /// <summary>
+    /// Interaction logic for TestFileListControl.xaml
+    /// </summary>
+    public partial class TestFileListControl : UserControl
+    {
+        public TestFileListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
