f457841734bf12e6dd674148949a1f6e96b2167c
tbircek <tbircek@beckwithelectric.com>
2020-01-22 09:37:57 -0500

    adds design-time model for multi-test interface

---
+++ metering.core/ViewModels/Test/Design/TestFileListItemDesignModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace metering.core
+{
+    /// <summary>
+    /// Design time data for a <see cref="TestFileListItemViewModel"/>
+    /// </summary>
+    public class TestFileItemDesignModel : TestFileListItemViewModel
+    {
+        #region Singleton       
+
+        /// <summary>
+        /// Single instance of the design time model
+        /// </summary>
+        public static TestFileItemDesignModel Instance => new TestFileItemDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public TestFileItemDesignModel()
+        {
+            IsDeletable = true;
+            TestToolTip = "Design time TestToolTip. This should show long version of file name and test status.";
+            DeleteTestToolTip = "Design time TestDeleteToolTip.";
+            TestStepBackgroundColor = "Transparent";
+            ShortTestFileName = "ShortDesign-time filename";
+        }
+
+        #endregion
+    }
+}
