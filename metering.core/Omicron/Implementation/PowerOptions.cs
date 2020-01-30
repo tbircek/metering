acb942897754b7884fcea401aa7e7f8d2d9a4e4b
tbircek <tbircek@beckwithelectric.com>
2020-01-30 12:04:30 -0500

    updates new async Tasks

--- metering.core/Omicron/Implementation/PowerOptions.cs
+++ metering.core/Omicron/Implementation/PowerOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using metering.core.Resources;
 
 namespace metering.core
@@ -8,6 +9,47 @@ namespace metering.core
     /// </summary>
     public class PowerOptions
     {
+
+        /// <summary>
+        /// Turns off outputs of Omicron Test Set and release it.
+        /// </summary>
+        public async Task TurnOffCMCAsync()
+        {
+            try
+            {
+                // lock the task
+                await AsyncAwaiter.AwaitAsync(nameof(TurnOffCMCAsync), async () =>
+                {
+
+                    // update the developer
+                    IoC.Logger.Log($"{nameof(TurnOffCMC)} started.", LogLevel.Informative);
+
+                    // send Turn off command to Omicron Test Set
+                    // await IoC.Task.Run(() => IoC.StringCommands.SendStringCommandAsync(OmicronStringCmd.out_ana_off));
+                    await IoC.StringCommands.SendStringCommandsAsync(OmicronStringCmd.out_ana_off);
+
+                    // update the developer
+                    IoC.Logger.Log($"{nameof(TurnOffCMC)} completed.", LogLevel.Informative);
+
+                });
+            }
+
+            catch (Exception ex)
+            {
+                // inform the developer about error.
+                IoC.Logger.Log($"InnerException: {ex.Message}");
+
+                // inform the user about error.
+                IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff}\tturnOffCMC setup: error detected.";
+
+                // catch inner exceptions if exists
+                if (ex.InnerException != null)
+                {
+                    // inform the user about more details about error.
+                    IoC.Communication.Log = $"{DateTime.Now.ToLocalTime():MM/dd/yy HH:mm:ss.fff}: Inner exception: {ex.InnerException}.";
+                }
+            }
+        }
 
         /// <summary>
         /// Turns off outputs of Omicron Test Set and release it.
@@ -24,7 +66,7 @@ namespace metering.core
                     IoC.Logger.Log($"{nameof(TurnOffCMC)} started.", LogLevel.Informative);
 
                     // send Turn off command to Omicron Test Set
-                    await IoC.Task.Run(() => IoC.StringCommands.SendStringCommandAsync(OmicronStringCmd.out_ana_off));
+                    await IoC.Task.Run(() => IoC.StringCommands.SendStringCommandsAsync(OmicronStringCmd.out_ana_off));
                     
                     // update the developer
                     IoC.Logger.Log($"{nameof(TurnOffCMC)} completed.", LogLevel.Informative);
@@ -64,7 +106,7 @@ namespace metering.core
                     IoC.Logger.Log($"{nameof(TurnOnCMC)} started.", LogLevel.Informative);
 
                     // Send command to Turn On Analog Outputs
-                    await IoC.Task.Run(() => IoC.StringCommands.SendStringCommandAsync(OmicronStringCmd.out_ana_on));
+                    await IoC.Task.Run(() => IoC.StringCommands.SendStringCommandsAsync(OmicronStringCmd.out_ana_on));
 
                     // update the developer
                     IoC.Logger.Log($"{nameof(TurnOnCMC)} completed.", LogLevel.Informative);
