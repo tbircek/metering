8f1d10fc24e4d55cf7ea9365b74ecfce53cc0e0a
tbircek <tbircek@beckwithelectric.com>
2020-02-17 14:04:23 -0500

    adds async version of existing methods

--- metering.core/Omicron/Implementation/InitialCMCSetup.cs
+++ metering.core/Omicron/Implementation/InitialCMCSetup.cs
@@ -23,10 +23,10 @@ namespace metering.core
         /// Sends Omicron commands to CMEngine
         /// </summary>
         /// <param name="CommandToSend">Omicron String Command</param>
-        private Task SendOmicronCommand(string CommandToSend)
+        private async Task<string> SendOmicronCommandAsync(string CommandToSend)
         {
             // send commands to Omicron Test Set
-            return IoC.Task.Run(async () => await IoC.StringCommands.SendStringCommandsAsync(omicronCommand: CommandToSend));
+            return await IoC.StringCommands.SendStringCommandsAsync(omicronCommand: CommandToSend);
 
         }
 
@@ -37,118 +37,18 @@ namespace metering.core
         /// <summary>
         /// Sets Omicron Test Set default values and limits.
         /// </summary>
-        [Obsolete("Please use Async version", true)]
-        public Task InitialSetup()
-        {
-            try
-            {
-                // Switches OFF all generators of a CMC immediately. 
-                SendOmicronCommand(OmicronStringCmd.out_ana_off);
-
-                // Clears the routings of all triples and the amplifier’s power supply that is turned off (clr) or not (clrnooff).
-                SendOmicronCommand(OmicronStringCmd.amp_route_clr);
-
-                // command to undefined an amplifier.. 
-                SendOmicronCommand(OmicronStringCmd.amp_def_clr);
-
-                // initiate max values
-                double MaxVoltageMagnitude = default;
-                double MaxCurrentMagnitude = default;
-
-                // Route Omicron amplifiers.
-                // voltage amplifiers
-                foreach (var config in IoC.TestDetails.SelectedVoltageConfiguration.ConfigIDs)
-                {
-                    // current hardware configuration location in the collection
-                    int hardwareConfigurationPosition = IoC.TestDetails.SelectedVoltageConfiguration.ConfigIDs.IndexOf(config);
-
-                    // current hardware configuration value in the collection
-                    int amp_no = Convert.ToInt32(IoC.TestDetails.SelectedVoltageConfiguration.AmplifierNumber[hardwareConfigurationPosition]);
-
-                    // route the amplifier to the user specification
-                    // amp_no == 1 is 1st,  amp_no == 5 is 2nd voltage amplifier per Omicron Documentation
-                    SendOmicronCommand(string.Format(OmicronStringCmd.amp_route_v_0_1, 5 == amp_no ? 2 : amp_no, config));
-
-                    // reset Max output value
-                    MaxVoltageMagnitude = IoC.TestDetails.SelectedVoltageConfiguration.MaxOutput[hardwareConfigurationPosition];
-
-                    // update Omicron voltage amplifier ranges.
-                    SendOmicronCommand(string.Format(OmicronStringCmd.amp_range_v_0_1, 5 == amp_no ? 2 : amp_no, MaxVoltageMagnitude));
-
-                }
-
-                // current amplifiers
-                foreach (var config in IoC.TestDetails.SelectedCurrentConfiguration.ConfigIDs)
-                {
-                    // current hardware configuration location in the collection
-                    int hardwareConfigurationPosition = IoC.TestDetails.SelectedCurrentConfiguration.ConfigIDs.IndexOf(config);
-
-                    // current hardware configuration value in the collection
-                    int amp_no = Convert.ToInt32(IoC.TestDetails.SelectedCurrentConfiguration.AmplifierNumber[hardwareConfigurationPosition]);
-
-                    // route the amplifier to the user specification
-                    // amp_no == 2 is 1st,  amp_no == 6 is 2nd current amplifier per Omicron Documentation
-                    SendOmicronCommand(string.Format(OmicronStringCmd.amp_route_i_0_1, 6 == amp_no ? 2 : 1, config));
-
-                    // reset Max output value
-                    MaxCurrentMagnitude = IoC.TestDetails.SelectedCurrentConfiguration.MaxOutput[hardwareConfigurationPosition];
-
-                    // update Omicron current amplifier ranges.
-                    SendOmicronCommand(string.Format(OmicronStringCmd.amp_range_i_0_1, 6 == amp_no ? 2 : 1, MaxCurrentMagnitude));
-
-                }
-
-                // update the user about the Omicron Test Set voltage connection scheme.
-                // 3x600V, 70VA @ 7.5V 10Arms
-                IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff} Voltage Configuration: {(string.IsNullOrWhiteSpace(IoC.TestDetails.SelectedVoltageConfiguration.WiringDiagramString) ? "Not used." : IoC.TestDetails.SelectedVoltageConfiguration.WiringDiagramString)}";
-
-                // update the user about the Omicron Test Set voltage connection scheme.
-                //  Selecting CConfig, 3x12.5A, 140VA @ 15V, 10Arms
-                IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff} Current Configuration: {(string.IsNullOrWhiteSpace(IoC.TestDetails.SelectedCurrentConfiguration.WiringDiagramString) ? "Not used." : IoC.TestDetails.SelectedCurrentConfiguration.WiringDiagramString)}";
-
-                // change power mode.
-                SendOmicronCommand(string.Format(OmicronStringCmd.out_ana_pmode_0, "timeabs"));
-
-                // return Task Completed
-                return Task.CompletedTask;
-
-            }
-            catch (Exception ex)
-            {
-                // update the log
-                IoC.Logger.Log($"InnerException: {ex.Message}");
-                IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff}\tinitial setup::Exception InnerException is : {ex.Message}.";
-
-                // catch inner exceptions if exists
-                if (ex.InnerException != null)
-                {
-                    // inform the user about more details about error.
-                    IoC.Communication.Log = $"{DateTime.Now.ToLocalTime():MM/dd/yy HH:mm:ss.fff}: Inner exception: {ex.InnerException}.";
-                }
-
-                // update Current Test File
-                IoC.Communication.UpdateCurrentTestFileListItem(CommunicationViewModel.TestStatus.Interrupted);
-
-                // return Task Completed
-                return Task.CompletedTask;
-            }
-        }
-
-        /// <summary>
-        /// Sets Omicron Test Set default values and limits.
-        /// </summary>
         public async Task InitialSetupAsync()
         {
             try
             {
                 // Switches OFF all generators of a CMC immediately. 
-                await IoC.Task.Run(() => SendOmicronCommand(OmicronStringCmd.out_ana_off));
+                await IoC.Task.Run(() => SendOmicronCommandAsync(OmicronStringCmd.out_ana_off));
 
                 // Clears the routings of all triples and the amplifier’s power supply that is turned off (clr) or not (clrnooff).
-                await IoC.Task.Run(() => SendOmicronCommand(OmicronStringCmd.amp_route_clr));
+                await IoC.Task.Run(() => SendOmicronCommandAsync(OmicronStringCmd.amp_route_clr));
 
                 // command to undefined an amplifier.. 
-                await IoC.Task.Run(() => SendOmicronCommand(OmicronStringCmd.amp_def_clr));
+                await IoC.Task.Run(() => SendOmicronCommandAsync(OmicronStringCmd.amp_def_clr));
 
                 // initiate max values
                 double MaxVoltageMagnitude = default;
@@ -166,13 +66,13 @@ namespace metering.core
 
                     // route the amplifier to the user specification
                     // amp_no == 1 is 1st,  amp_no == 5 is 2nd voltage amplifier per Omicron Documentation
-                    await IoC.Task.Run(() => SendOmicronCommand(string.Format(OmicronStringCmd.amp_route_v_0_1, 5 == amp_no ? 2 : amp_no, config)));
+                    await IoC.Task.Run(() => SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_route_v_0_1, 5 == amp_no ? 2 : amp_no, config)));
 
                     // reset Max output value
                     MaxVoltageMagnitude = await IoC.Task.Run(() => IoC.TestDetails.SelectedVoltageConfiguration.MaxOutput[hardwareConfigurationPosition]);
 
                     // update Omicron voltage amplifier ranges.
-                    await IoC.Task.Run(() => SendOmicronCommand(string.Format(OmicronStringCmd.amp_range_v_0_1, 5 == amp_no ? 2 : amp_no, MaxVoltageMagnitude)));
+                    await IoC.Task.Run(() => SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_range_v_0_1, 5 == amp_no ? 2 : amp_no, MaxVoltageMagnitude)));
 
                 }
 
@@ -187,13 +87,13 @@ namespace metering.core
 
                     // route the amplifier to the user specification
                     // amp_no == 2 is 1st,  amp_no == 6 is 2nd current amplifier per Omicron Documentation
-                    await IoC.Task.Run(() => SendOmicronCommand(string.Format(OmicronStringCmd.amp_route_i_0_1, 6 == amp_no ? 2 : 1, config)));
+                    await IoC.Task.Run(() => SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_route_i_0_1, 6 == amp_no ? 2 : 1, config)));
 
                     // reset Max output value
                     MaxCurrentMagnitude = await IoC.Task.Run(() => IoC.TestDetails.SelectedCurrentConfiguration.MaxOutput[hardwareConfigurationPosition]);
 
                     // update Omicron current amplifier ranges.
-                    await IoC.Task.Run(() => SendOmicronCommand(string.Format(OmicronStringCmd.amp_range_i_0_1, 6 == amp_no ? 2 : 1, MaxCurrentMagnitude)));
+                    await IoC.Task.Run(() => SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_range_i_0_1, 6 == amp_no ? 2 : 1, MaxCurrentMagnitude)));
 
                 }
 
@@ -206,29 +106,117 @@ namespace metering.core
                 IoC.Communication.Log = await IoC.Task.Run(() => $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff} Current Configuration: {(string.IsNullOrWhiteSpace(IoC.TestDetails.SelectedCurrentConfiguration.WiringDiagramString) ? "Not used." : IoC.TestDetails.SelectedCurrentConfiguration.WiringDiagramString)}");
 
                 // change power mode.
-                await IoC.Task.Run(() => SendOmicronCommand(string.Format(OmicronStringCmd.out_ana_pmode_0, "timeabs")));
+                await IoC.Task.Run(() => SendOmicronCommandAsync(string.Format(OmicronStringCmd.out_ana_pmode_0, "timeabs")));
 
             }
             catch (Exception)
             {
-                //// update the log
-                //IoC.Logger.Log($"InnerException: {ex.Message}");
-                //IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff}\tinitial setup::Exception InnerException is : {ex.Message}.";
-
-                //// catch inner exceptions if exists
-                //if (ex.InnerException != null)
-                //{
-                //    // inform the user about more details about error.
-                //    IoC.Communication.Log = $"{DateTime.Now.ToLocalTime():MM/dd/yy HH:mm:ss.fff}: Inner exception: {ex.InnerException}.";
-                //}
-
-                //// update Current Test File
-                //IoC.Communication.UpdateCurrentTestFileListItem(CommunicationViewModel.TestStatus.Interrupted);   
-
-                // Trying to stop the app gracefully.
-                await IoC.Task.Run(() => IoC.ReleaseOmicron.ProcessErrorsAsync(false));
+                // re-throw error
+                throw;
             }
         }
+
+
+        ///// <summary>
+        ///// Sets Omicron Test Set default values and limits.
+        ///// </summary>
+        //[Obsolete("Please use Async version", true)]
+        //public Task InitialSetup()
+        //{
+        //    try
+        //    {
+        //        // Switches OFF all generators of a CMC immediately. 
+        //        SendOmicronCommandAsync(OmicronStringCmd.out_ana_off);
+
+        //        // Clears the routings of all triples and the amplifier’s power supply that is turned off (clr) or not (clrnooff).
+        //        SendOmicronCommandAsync(OmicronStringCmd.amp_route_clr);
+
+        //        // command to undefined an amplifier.. 
+        //        SendOmicronCommandAsync(OmicronStringCmd.amp_def_clr);
+
+        //        // initiate max values
+        //        double MaxVoltageMagnitude = default;
+        //        double MaxCurrentMagnitude = default;
+
+        //        // Route Omicron amplifiers.
+        //        // voltage amplifiers
+        //        foreach (var config in IoC.TestDetails.SelectedVoltageConfiguration.ConfigIDs)
+        //        {
+        //            // current hardware configuration location in the collection
+        //            int hardwareConfigurationPosition = IoC.TestDetails.SelectedVoltageConfiguration.ConfigIDs.IndexOf(config);
+
+        //            // current hardware configuration value in the collection
+        //            int amp_no = Convert.ToInt32(IoC.TestDetails.SelectedVoltageConfiguration.AmplifierNumber[hardwareConfigurationPosition]);
+
+        //            // route the amplifier to the user specification
+        //            // amp_no == 1 is 1st,  amp_no == 5 is 2nd voltage amplifier per Omicron Documentation
+        //            SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_route_v_0_1, 5 == amp_no ? 2 : amp_no, config));
+
+        //            // reset Max output value
+        //            MaxVoltageMagnitude = IoC.TestDetails.SelectedVoltageConfiguration.MaxOutput[hardwareConfigurationPosition];
+
+        //            // update Omicron voltage amplifier ranges.
+        //            SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_range_v_0_1, 5 == amp_no ? 2 : amp_no, MaxVoltageMagnitude));
+
+        //        }
+
+        //        // current amplifiers
+        //        foreach (var config in IoC.TestDetails.SelectedCurrentConfiguration.ConfigIDs)
+        //        {
+        //            // current hardware configuration location in the collection
+        //            int hardwareConfigurationPosition = IoC.TestDetails.SelectedCurrentConfiguration.ConfigIDs.IndexOf(config);
+
+        //            // current hardware configuration value in the collection
+        //            int amp_no = Convert.ToInt32(IoC.TestDetails.SelectedCurrentConfiguration.AmplifierNumber[hardwareConfigurationPosition]);
+
+        //            // route the amplifier to the user specification
+        //            // amp_no == 2 is 1st,  amp_no == 6 is 2nd current amplifier per Omicron Documentation
+        //            SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_route_i_0_1, 6 == amp_no ? 2 : 1, config));
+
+        //            // reset Max output value
+        //            MaxCurrentMagnitude = IoC.TestDetails.SelectedCurrentConfiguration.MaxOutput[hardwareConfigurationPosition];
+
+        //            // update Omicron current amplifier ranges.
+        //            SendOmicronCommandAsync(string.Format(OmicronStringCmd.amp_range_i_0_1, 6 == amp_no ? 2 : 1, MaxCurrentMagnitude));
+
+        //        }
+
+        //        // update the user about the Omicron Test Set voltage connection scheme.
+        //        // 3x600V, 70VA @ 7.5V 10Arms
+        //        IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff} Voltage Configuration: {(string.IsNullOrWhiteSpace(IoC.TestDetails.SelectedVoltageConfiguration.WiringDiagramString) ? "Not used." : IoC.TestDetails.SelectedVoltageConfiguration.WiringDiagramString)}";
+
+        //        // update the user about the Omicron Test Set voltage connection scheme.
+        //        //  Selecting CConfig, 3x12.5A, 140VA @ 15V, 10Arms
+        //        IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff} Current Configuration: {(string.IsNullOrWhiteSpace(IoC.TestDetails.SelectedCurrentConfiguration.WiringDiagramString) ? "Not used." : IoC.TestDetails.SelectedCurrentConfiguration.WiringDiagramString)}";
+
+        //        // change power mode.
+        //        SendOmicronCommandAsync(string.Format(OmicronStringCmd.out_ana_pmode_0, "timeabs"));
+
+        //        // return Task Completed
+        //        return Task.CompletedTask;
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // update the log
+        //        IoC.Logger.Log($"InnerException: {ex.Message}");
+        //        IoC.Communication.Log = $"Time: {DateTime.Now.ToLocalTime():MM/dd/yy hh:mm:ss.fff}\tinitial setup::Exception InnerException is : {ex.Message}.";
+
+        //        // catch inner exceptions if exists
+        //        if (ex.InnerException != null)
+        //        {
+        //            // inform the user about more details about error.
+        //            IoC.Communication.Log = $"{DateTime.Now.ToLocalTime():MM/dd/yy HH:mm:ss.fff}: Inner exception: {ex.InnerException}.";
+        //        }
+
+        //        // update Current Test File
+        //        IoC.Communication.UpdateCurrentTestFileListItem(CommunicationViewModel.TestStatus.Interrupted);
+
+        //        // return Task Completed
+        //        return Task.CompletedTask;
+        //    }
+        //}
+
     }
 
     #endregion
